70d22e025d3cb2bf987e996bf59b66bdbe20d3e8
JoeHeap <jheap@spartaglobal.com>
2026-03-20 10:24:02 +0000

    setup finished

---
+++ src/test/java/com/sparta/TestRunner.java
@@ -0,0 +1,15 @@
+package com.sparta;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com.sparta.stepdefinitions",
+        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        monochrome = true
+)
+
+public class TestRunner {
+}
